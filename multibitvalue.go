b83b7d923dcb506f7695f0a0daea038ed23d23cc
Nigel Drego <ndrego@gmail.com>
2016-05-15 23:24:07 -0700

    Add MultiBitValue
    Add multi-bit value functionality, with some unary ops (~, |, ~|, &,
    ~&). Still to be added: parity check (^, ~^) and all binary ops.

---
+++ multibitvalue.go
@@ -0,0 +1,261 @@
+package sysgo
+
+import (
+	_ "fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+type mbv64 struct {
+	numBits uint
+	bits uint64
+	hiz uint64
+	undef uint64
+}
+
+type mbvBig struct {
+	numBits uint
+	bits big.Int
+	hiz big.Int
+	undef big.Int
+}
+
+type MultiBitValue interface {
+	bitLen() uint
+	getBit(uint) LogicValue
+	setBit(uint, LogicValue)
+	unary(string) MultiBitValue
+	binary(string, MultiBitValue) MultiBitValue
+}
+
+
+func (X *mbv64) copy() (Z *mbv64) {
+	Z = NewMultiBitValue(X.bitLen()).(*mbv64)
+	Z.bits = X.bits
+	Z.hiz = X.hiz
+	Z.undef = X.undef
+
+	return
+}
+
+func (X *mbvBig) copy() (Z *mbvBig) {
+	Z = NewMultiBitValue(X.bitLen()).(*mbvBig)
+	Z.bits.SetBytes(X.bits.Bytes())
+	Z.hiz.SetBytes(X.hiz.Bytes())
+	Z.undef.SetBytes(X.undef.Bytes())
+
+	return
+}
+
+func (X *mbv64) bitLen() uint {
+	return X.numBits
+}
+
+func (X *mbvBig) bitLen() uint {
+	return X.numBits
+}
+
+func (X *mbv64) getBit(b uint) LogicValue {
+	if b > (X.numBits - 1) {
+		log.Fatal("Index (%d) out of bounds.\n", b)
+	}
+
+	mask := uint64(1 << b)
+
+	if X.undef & mask != 0 {
+		return Undefined
+	} else if X.hiz & mask > 0 {
+		return HiZ
+	} else {
+		return LogicValue((X.bits >> b) & 0x1)
+	}
+}
+
+func (X *mbvBig) getBit(b uint) LogicValue {
+	if b > (X.numBits - 1) {
+		log.Fatal("Index (%d) out of bounds.\n", b)
+	}
+
+	if X.undef.Bit(int(b)) == 1 {
+		return Undefined
+	} else if X.hiz.Bit(int(b)) == 1 {
+		return HiZ
+	} else {
+		return LogicValue(X.bits.Bit(int(b)))
+	}
+}
+
+
+func (X *mbv64) setBit(b uint, v LogicValue) {
+	if b > (X.numBits - 1) {
+		log.Fatal("Index (%d) out of bounds.\n", b)
+	}
+
+	mask := uint64(1 << b)
+	
+	switch v {
+	case Undefined:
+		X.undef |= mask
+		X.hiz &= ^mask
+	case HiZ:
+		X.hiz |= mask
+		X.undef &= ^mask
+	case Hi:
+		X.bits |= mask
+		X.hiz &= ^mask
+		X.undef &= ^mask
+	case Lo:
+		X.bits &= ^mask
+		X.hiz &= ^mask
+		X.undef &= ^mask
+	}
+}
+
+func (X *mbvBig) setBit(b uint, v LogicValue) {
+	if b > (X.numBits - 1) {
+		log.Fatal("Index (%d) out of bounds.\n", b)
+	}
+
+	var mask big.Int
+	mask.SetBit(&mask, int(b), 1)
+	
+	switch v {
+	case Undefined:
+		X.undef.Or(&X.undef, &mask)
+		X.hiz.AndNot(&X.hiz, &mask)
+	case HiZ:
+		X.hiz.Or(&X.hiz, &mask)
+		X.undef.AndNot(&X.undef, &mask)
+	case Hi:
+		X.bits.Or(&X.bits, &mask)
+		X.hiz.AndNot(&X.hiz, &mask)
+		X.undef.AndNot(&X.undef, &mask)
+	case Lo:
+		X.bits.AndNot(&X.bits, &mask)
+		X.hiz.AndNot(&X.hiz, &mask)
+		X.undef.AndNot(&X.undef, &mask)
+	}
+}
+
+
+func (X *mbv64) unary(op string) MultiBitValue {
+	mask := uint64(1 << X.numBits - 1)
+
+	switch op {
+	case "~":
+		Z := X.copy()
+		Z.bits = ^Z.bits & mask
+		return Z
+	case "|", "~|":
+		Z := NewMultiBitValue(1)
+		if X.hiz & mask != 0 || X.undef & mask != 0 {
+			Z.setBit(0, Undefined)
+		} else {
+			if X.bits & mask != 0 {
+				Z.setBit(0, Hi)
+			} else {
+				Z.setBit(0, Lo)
+			}
+
+			if op == "~|" {
+				Z.setBit(0, Z.getBit(0).Unary('~'))
+			}
+		}
+		return Z
+	case "&", "~&":
+		Z := NewMultiBitValue(1)
+		if X.hiz & mask != 0 || X.undef & mask != 0 {
+			Z.setBit(0, Undefined)
+		} else {
+			if X.bits == mask {
+				Z.setBit(0, Hi)
+			} else {
+				Z.setBit(0, Lo)
+			}
+
+			if op == "~&" {
+				Z.setBit(0, Z.getBit(0).Unary('~'))
+			}
+		}
+		return Z
+	default:
+		return X
+	}
+
+}
+
+func (X *mbvBig) unary(op string) MultiBitValue {
+	var zero big.Int
+	switch op {
+	case "~":
+		Z := X.copy()
+		Z.bits.Not(&Z.bits)
+		return Z
+	case "|", "~|":
+		Z := NewMultiBitValue(1)
+		if X.hiz.Cmp(&zero) != 0 || X.undef.Cmp(&zero) != 0 {
+			Z.setBit(0, Undefined)
+		} else {
+			if X.bits.Cmp(&zero) != 0 {
+				Z.setBit(0, Hi)
+			} else {
+				Z.setBit(0, Lo)
+			}
+			if op == "~|" {
+				Z.setBit(0, Z.getBit(0).Unary('~'))
+			}
+		}
+		return Z
+	case "&", "~&":
+		Z := NewMultiBitValue(1)
+		if X.hiz.Cmp(&zero) != 0 || X.undef.Cmp(&zero) != 0 {
+			Z.setBit(0, Undefined)
+		} else {
+			var mask big.Int
+			mask.Sub(mask.Exp(big.NewInt(2), big.NewInt(int64(X.numBits)), nil), big.NewInt(1))
+			if X.bits.Cmp(&mask) == 0 {
+				Z.setBit(0, Hi)
+			} else {
+				Z.setBit(0, Lo)
+			}
+			if op == "~&" {
+				Z.setBit(0, Z.getBit(0).Unary('~'))
+			}
+		}
+		return Z
+	default:
+		return X
+	}
+
+}
+
+
+func (X *mbv64) binary(op string, Y MultiBitValue) (Z MultiBitValue) {	
+	switch op {
+	case "&":
+		Z = NewMultiBitValue(uint(math.Max(float64(X.numBits), float64(Y.bitLen()))))
+	case "+":
+	}
+
+	return
+}
+
+func (X *mbvBig) binary(op string, Y MultiBitValue) (Z MultiBitValue) {
+	return
+}
+
+func NewMultiBitValue(numBits uint) MultiBitValue {
+	switch {
+	case numBits <= 64:
+		mbv := new(mbv64)
+		mbv.numBits = numBits
+		return mbv
+	case numBits > 64:
+		mbv := new(mbvBig)
+		mbv.numBits = numBits
+		return mbv
+	default:
+		return nil
+	}
+}
---
+++ multibitvalue_test.go
@@ -0,0 +1,96 @@
+package sysgo
+
+import (
+	_ "fmt"
+	"github.com/stretchr/testify/assert"
+	_ "math"
+	_ "reflect"
+	"testing"
+)
+
+func TestMBVGetSetBit(t *testing.T) {
+	mbv, ok := NewMultiBitValue(37).(*mbv64)
+	assert.Equal(t, true, ok, "mbv not an mbv64")
+	
+	// Set lo/hi first
+	mbv.setBit(0, Hi)
+	assert.Equal(t, uint64(0x1), mbv.bits, "Bit 0 not Hi")
+	assert.Equal(t, Hi, mbv.getBit(0), "Bit 0 not Hi")
+
+	mbv.setBit(36, Hi)
+	assert.Equal(t, uint64(0x1000000001), mbv.bits, "Bit 36 not Hi")
+	assert.Equal(t, Hi, mbv.getBit(36), "Bit 36 not Hi")
+
+	mbv.setBit(35, HiZ)
+	assert.Equal(t, uint64(0x1000000001), mbv.bits, "Bit 35 not HiZ")
+	assert.Equal(t, uint64(0x0800000000), mbv.hiz, "Bit 35 not Hiz")
+	assert.Equal(t, HiZ, mbv.getBit(35), "Bit 35 not HiZ")
+
+	mbv.setBit(13, Undefined)
+	assert.Equal(t, uint64(0x1000000001), mbv.bits, "Bit 35 not HiZ")
+	assert.Equal(t, uint64(0x2000), mbv.undef, "Bit 13 not Undefined")
+	assert.Equal(t, Undefined, mbv.getBit(13), "Bit 13 not Undefined")
+
+	mbv.setBit(13, Lo)
+	assert.Equal(t, uint64(0x1000000001), mbv.bits, "Bit 13 not Lo")
+	assert.Equal(t, uint64(0x0), mbv.undef, "Bit 13 not Lo")
+	assert.Equal(t, uint64(0x800000000), mbv.hiz, "Bit 13 not Lo")
+	assert.Equal(t, Lo, mbv.getBit(13), "Bit 13 not Lo")
+	
+	mbv.setBit(36, HiZ)
+	assert.Equal(t, uint64(0x1000000001), mbv.bits, "Bit 36 not Hi")
+	assert.Equal(t, uint64(0x1800000000), mbv.hiz, "Bit 36 not HiZ")
+	assert.Equal(t, HiZ, mbv.getBit(36), "Bit 36 not HiZ")
+
+	mbv.setBit(0, Lo)
+	assert.Equal(t, uint64(0x1000000000), mbv.bits, "Bit 0 not Lo")
+	assert.Equal(t, Lo, mbv.getBit(0), "Bit 0 not Lo")
+	
+}
+
+func TestMBVUnaryOps(t *testing.T) {
+	var mbv MultiBitValue
+	sizes := []uint{61, 247}
+
+	for _, size := range sizes {
+		mbv = NewMultiBitValue(size)
+
+		switch {
+		case size <= 64:
+			_, ok := mbv.(*mbv64)
+			assert.Equal(t, true, ok, "Wrong kind")
+		case size > 64:
+			_, ok := mbv.(*mbvBig)
+			assert.Equal(t, true, ok, "Wrong kind")
+		}
+
+		mbv.setBit(size - 2, Hi)
+		inv := mbv.unary("~")
+
+		assert.Equal(t, Lo, inv.getBit(size - 2), "inv incorrect")
+	
+		or := mbv.unary("|")
+		assert.Equal(t, uint(1), or.bitLen(), "or has more than 1 bit")
+		assert.Equal(t, Hi, or.getBit(0), "Bit 0 should be Hi")
+
+		nor := mbv.unary("~|")
+		assert.Equal(t, uint(1), nor.bitLen(), "nor has more than 1 bit")
+		assert.Equal(t, Lo, nor.getBit(0), "Bit 0 should be Lo")
+	
+		and := mbv.unary("&")
+		assert.Equal(t, uint(1), and.bitLen(), "and has more than 1 bit")
+		assert.Equal(t, Lo, and.getBit(0), "Bit 0 should be Lo")
+
+		for i := uint(0); i < mbv.bitLen(); i++ {
+			mbv.setBit(i, Hi)
+		}
+		and = mbv.unary("&")
+		assert.Equal(t, uint(1), and.bitLen(), "and has more than 1 bit")
+		assert.Equal(t, Hi, and.getBit(0), "Bit 0 should be Hi")
+
+		nand := mbv.unary("~&")
+		assert.Equal(t, uint(1), nand.bitLen(), "nand has more than 1 bit")
+		assert.Equal(t, Lo, nand.getBit(0), "Bit 0 should be Lo")
+	}
+
+}
